d82b071c7747e564ec76c0bcf83c001bd2ba6e7a
t1murius <timur.yagudin.08@gmail.com>
2022-06-12 22:56:14 +0400

    Now you can control the snake!

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ function drawSqr(color, size, position){
     cs.fillRect(position[0] * size, position[1] * size, size, size);
 }
 
+document.addEventListener('keydown', function(event){
+    switch(event.key){
+        case "w":
+            if(vectorY != 1){vectorX = 0, vectorY = -1;}
+            break;
+        case "a":
+            if(vectorX != 1){vectorX = -1, vectorY = 0;}
+            break;
+        case "s":
+            if(vectorY != -1){vectorX = 0, vectorY = 1;}
+            break;
+        case "d":
+            if(vectorX != -1){vectorX = 1, vectorY = 0;}
+            break;
+    }
+return;
+});
+
+
 const sqrSize = 20;
 cs.fillStyle = "#000000";
 cs.fillRect(0, 0, canvas.width, canvas.height);
 
+let vectorX = 1, vectorY = 0;
 
 let snake = {
     position : [
@@ -19,15 +39,17 @@ let snake = {
     ],
     color: "#00FFFF",
     snakeRender: function(event){
+            
         let [x, y] = this.position.at(-1);
-        this.position.push([x + 1, y]);
+        this.position.push([x + vectorX, y + vectorY]);
         let oldTail = this.position.shift()
         console.log(this.position)
+        console.log(vectorX, vectorY)
         drawSqr("#000000", sqrSize, oldTail);
         for(let pos of this.position){
             drawSqr(this.color, sqrSize, pos);
         }
-        setTimeout(() => snake.snakeRender(), 1000);
+        setTimeout(() => snake.snakeRender(), 100);
     }
 };
 
