f84137fbe759bcfc73907455aa16d71c7dc6376d
t1murius <timur.yagudin.08@gmail.com>
2022-06-18 12:43:47 +0400

    I`ve added some animations, and now game over looks a little better

--- visual.js
+++ visual.js
@@ -3,8 +3,24 @@ const score = document.getElementById("score");
 
 function visualGameOver(){
     text.innerText = "Game over";// we show string "Game over" 
-    let margin = text.style.marginTop;
-    for(let i = 0; i <= 20; i++){
-        setTimeout(() => text.style.marginTop = (parseInt(margin) - i) + "px", 1000);
+    let time = 0;
+    let margin = parseInt(text.style.marginTop);
+    let id = null;
+    clearInterval(id);
+    id = setInterval(waiting, 100)
+    
+    function textAnimation() {
+        if (margin == 290) {
+            score.innerText = `Total score: ${snake.score}`;
+            clearInterval(id);
+        } else { 
+            text.style.marginTop = --margin + "px";
+        }
+    }
+    function waiting(){
+        if(time == 10){
+            id = setInterval(textAnimation, 10);
+        }
+        time++
     }
 }
